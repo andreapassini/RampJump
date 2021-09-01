be39b1c830d1f11689a22e3a4bd758f346c9ca0b
Andrea Passini <andrea.passini@studenti.unimi.it>
2021-09-01 17:06:03 +0200

    Health system beta

---
+++ 2D RampJump/Assets/Scripts/Player Health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    #region Variables
+    public GameObject losePanel;
+
+    public Text healthDisplay;
+    public int health;
+    #endregion
+
+    #region Unity Methods
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        //Add sound to demage taken
+    
+        //source.Play();
+        this.health -= damageAmount;
+        healthDisplay.text = health.ToString();
+            
+        //Se la vita è <0, allora elimina player
+        if (this.health <= 0) {
+            healthDisplay.text = "0";
+            losePanel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
