3b829db3bf5739f6fb1a2652d6d18bba4b94cb4e
Andrea Passini <andrea.passini@studenti.unimi.it>
2021-09-03 12:36:26 +0200

    Health Pick-Up working

---
+++ 2D RampJump/Assets/Scripts/HealthPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour
+{
+	#region Variables
+	public int health = 50;
+
+	#endregion
+
+	#region Unity Methods
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player") {
+			collision.GetComponent<PlayerHealth>().TakeHealth(health);
+			Destroy(gameObject);
+		}
+	}
+
+	#endregion
+}
