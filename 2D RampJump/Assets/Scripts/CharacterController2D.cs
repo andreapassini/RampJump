4ff28f1758acfd09e80fe5e8cef1067484f53135
Andrea Passini <andrea.passini@studenti.unimi.it>
2021-11-23 18:45:34 +0100

    Movement Script optimization

--- 2D RampJump/Assets/Scripts/CharacterController2D.cs
+++ 2D RampJump/Assets/Scripts/CharacterController2D.cs
@@ -139,8 +139,13 @@ public class CharacterController2D : MonoBehaviour
 		m_FacingRight = !m_FacingRight;
 
 		// Multiply the player's x local scale by -1.
+		/*
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+		*/
+
+		transform.Rotate(0f, 180f, 0f);
 	}
+
 }
--- 2D RampJump/Assets/Scripts/Movement.cs
+++ 2D RampJump/Assets/Scripts/Movement.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Movement : MonoBehaviour
 {
-	[SerializeField] float speed = 100f;
+	[SerializeField] float runSpeed = 100f;
 	[SerializeField] float jumpForce;
-	private float input;
+	private float horizontalMove;
 	private bool isJumping, isGrounded;
 
 	private Rigidbody2D rb;
+	public Animator animator;
 
 	[SerializeField] LayerMask whatIsGround;
 	[SerializeField] Transform groundCheck;
 	const float groundedRadius = .2f;
+	private bool grounded; // Whether or not the player is grounded.
 
+	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+	[Header("Events")]
+	[Space]
+
+	public UnityEvent OnLandEvent;
 
 	private void Awake()
 	{
@@ -25,14 +34,20 @@ public class Movement : MonoBehaviour
 
 	private void Update()
 	{
-		input = Input.GetAxisRaw("Horizontal");
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 		//Potrei anche usare un vettore normalizzato
 
-		Flip(input);
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+		if (Input.GetButtonDown("Jump") && isGrounded) {
 			isJumping = true;
+			animator.SetBool("IsJumping", true);
 		}
+	}
+
+	public void OnLanding()
+	{
+		animator.SetBool("IsJumping", false);
 	}
 
 	private void FixedUpdate()
@@ -41,23 +56,48 @@ public class Movement : MonoBehaviour
 		isGrounded = false;
 		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
 
-		rb.velocity = new Vector2(input * speed, rb.velocity.y);
+		rb.velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, rb.velocity.y);
 
 		if (isJumping) {
-			//rb.velocity = Vector2.up * jumpForce;
+			
 			rb.AddForce(transform.up * (jumpForce), ForceMode2D.Impulse);
+			//rb.AddForce(new Vector2(0f, jumpForce));
 			isJumping = false;
+		}
+
+		// If the input is moving the player right and the player is facing left...
+		if (horizontalMove > 0 && !m_FacingRight) {
+			// ... flip the player.
+			Flip();
+		}
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if (horizontalMove < 0 && m_FacingRight) {
+			// ... flip the player.
+			Flip();
+		}
+
+		bool wasGrounded = grounded;
+		grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+		for (int i = 0; i < colliders.Length; i++) {
+			if (colliders[i].gameObject != gameObject) {
+				grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
 		}
 	}
 
 
 
-	private void Flip(float input)
+	private void Flip()
 	{
-		if (input > 0) {
-			transform.eulerAngles = new Vector3(0, 0, 0);
-		} else if (input < 0) {
-			transform.eulerAngles = new Vector3(0, 180, 0);
-		}
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		transform.Rotate(0f, 180f, 0f);
 	}
 }
