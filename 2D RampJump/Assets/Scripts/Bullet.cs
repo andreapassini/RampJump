ee2ceb593d26cc9d39686c1191a24ef06a0840c7
Andrea Passini <andrea.passini@studenti.unimi.it>
2021-11-25 12:00:46 +0100

    New Level For AI

--- 2D RampJump/Assets/Scripts/Bullet.cs
+++ 2D RampJump/Assets/Scripts/Bullet.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-	[SerializeField] GameObject hitGroundEffect;
-	[SerializeField] GameObject hitDmgEffect;
+	#region Variables
+
+	public GameObject hitGroundEffect;
+	public GameObject hitDmgEffect;
 
 	[SerializeField] float destroyAfterTime = 5f;
+
+	public string target = "Enemy";
+	public int damage = 10;
+
+	#endregion
+
+	#region Unity Methods
 
 	private void Start()
 	{
 		StartCoroutine(DestroyBulletAfterTime());
 	}
 
-	private void OnCollisionEnter2D(Collision2D collision)
+	public void OnCollisionEnter2D(Collision2D collision)
 	{
+		GameObject effect;
+
 		//Effetto diverso a seconda di cosa si colpisce
-		/*
-		
-		if (_rigidbody2D.tag == "Ground") {
-			GameObject effect = Instantiate(hitGroundEffect, transform.position, Quaternion.identity);
-			Destroy(effect, 5f);
+		if (collision.gameObject.tag == target) {
+			collision.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+			effect = Instantiate(hitDmgEffect, transform.position, Quaternion.identity);
+		} else {
+			effect = Instantiate(hitGroundEffect, transform.position, Quaternion.identity);
 		}
 
-		if (_rigidbody2D.tag == "Damage") {
-			GameObject effect = Instantiate(hitDmgEffect, transform.position, Quaternion.identity);
-			Destroy(effect, 5f);
-		}
-
-		*/
-		
-		GameObject effect = Instantiate(hitGroundEffect, transform.position, Quaternion.identity);
 		Destroy(effect, 2f);
 		Destroy(gameObject);
 	}
@@ -41,5 +44,8 @@ public class Bullet : MonoBehaviour
 		yield return new WaitForSeconds(destroyAfterTime);
 		Destroy(gameObject);
 	}
+
+	#endregion
+
 }
 
