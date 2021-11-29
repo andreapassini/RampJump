416ed038716e405f5f443d3d0f0d9ee86acf1e31
Andrea Passini <andrea.passini@studenti.unimi.it>
2021-11-29 19:09:54 +0100

    Spawning Points, Nodes and Root

---
+++ 2D RampJump/Assets/Scripts/SpawningController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningController : MonoBehaviour
+{
+    #region Variables
+
+    public Transform[] spwanPoints;
+    public GameObject[] enemies;
+
+
+    #endregion
+
+    #region Unity Methods
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
